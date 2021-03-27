c38e76304ae7ad1c7d49ed4178f981091558d11e
Krasi Zorbov <krasi.zorbov@abv.bg>
2021-03-27 14:52:59 +0200

    Refactoring

--- Racoon6/client/src/components/paypal/Payment.js
+++ Racoon6/client/src/components/paypal/Payment.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { PayPalButton } from 'react-paypal-button-v2';
 
-export default class PayPal extends React.Component {
+export default class Payment extends React.Component {
   render() {
     return (
       <PayPalButton
-        amount='25.00'
+        amount='1.00'
         // shippingPreference="NO_SHIPPING" // default is "GET_FROM_FILE"
         onSuccess={(details, data) => {
           alert('Transaction completed by ' + details.payer.name.given_name);
@@ -16,9 +16,12 @@ export default class PayPal extends React.Component {
           return fetch('/paypal-transaction-complete', {
             method: 'post',
             body: JSON.stringify({
-              orderID: data.orderID,
+              orderId: data.orderID,
             }),
           });
+        }}
+        options={{
+          clientId: 'PRODUCTION_CLIENT_ID',
         }}
       />
     );
